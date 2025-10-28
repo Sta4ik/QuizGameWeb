ef187c6c2d8727d7205e3828f6961f648fad23cc
Sta4ik <sta4ik@tutamail.com>
2025-10-28 12:56:16 +0300

    update logic

--- question.js
+++ question.js
@@ -1,21 +1,25 @@
-export class Question {
-    constructor(question, A, B, C, D, correctAnswer)
-    {
-        this.question = question;
-        this.answers = [A, B, C, D];
-        this.correctAnswer = correctAnswer;
-    }
-
-    showQuestion(questionP, answerButtons){
-        questionP.textContent = this.question;
-
-        for(let i = 0; i < this.answers; ++i)
-        {
-            answerButtons[i].textContent = this.answers[i];
-        }
-    }
-
-    isCorrect(selected){
-        return selected === this.correctAnswer;
+export class Questions{
+    constructor(questions, answer, correct){
+        this.questions = questions;
+        this.answer = answer; 
+        this.correct = correct;
     }
 }
+
+export let arrayQuiz = [
+    {
+        question: 'First question',
+        answerText: ["A", "B", "C", "D"],
+        correct: 1,
+    },
+    {
+        question: 'Second question',
+        answerText: ["Z", "X", "C", "V"],
+        correct: 0,
+    },    
+    {
+        question: 'Third question',
+        answerText: ["J", "K", "L", "P"],
+        correct: 3,
+    },
+]
